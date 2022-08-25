32fc0ae3600c0894ed79b4216e50bfe4b9ff7ffd
Daniel2231 <daniel2231.dev@gmail.com>
2022-08-25 09:36:45 +0900

    socket hotfix

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,10 +1,11 @@
 // Dedicated socket file for socket connection
 // This is to prevent multiple socket connection
 // Using react context to share socket connection
-import React from "react";
-import io from "socket.io-client";
+import React from 'react';
+import io from 'socket.io-client';
 
 export const socket = io(`${process.env.REACT_APP_SOCKET_URL}`, {
-    withCredentials: true,
+	withCredentials: true,
+	transports: ['websocket'],
 });
 export const SocketContext = React.createContext(socket);

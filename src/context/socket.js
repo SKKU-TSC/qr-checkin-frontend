fcc29028f1da776dd0583f06e7d322a7cf0aa8ab
Daniel2231 <daniel2231.dev@gmail.com>
2022-08-25 09:49:13 +0900

    revert changes

--- src/context/socket.js
+++ src/context/socket.js
@@ -6,6 +6,5 @@ import io from 'socket.io-client';
 
 export const socket = io(`${process.env.REACT_APP_SOCKET_URL}`, {
 	withCredentials: true,
-	transports: ['websocket'],
 });
 export const SocketContext = React.createContext(socket);

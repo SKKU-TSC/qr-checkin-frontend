4db0bb4bc01bee6e645d170da91529c2b79ef716
Daniel2231 <daniel2231.dev@gmail.com>
2023-07-04 21:03:49 +0900

    changed api

--- src/api/users.js
+++ src/api/users.js
@@ -1,79 +1,76 @@
-import { userValidation } from "./validations";
-import axios from "axios";
+import { userValidation } from './validations';
+import axios from 'axios';
 
 export const getAllUsers = async () => {
-  try {
-    const result = await axios.get(`${process.env.REACT_APP_API_URL}/users`);
-    return result;
-  } catch (error) {
-    throw new Error(error.message);
-  }
+	try {
+		await axios.get(`${process.env.REACT_APP_API_URL}/users`).then((res) => {
+			return res.data;
+		});
+	} catch (error) {
+		throw new Error(error.message);
+	}
 };
 
 export const getOneUser = async (studentId) => {
-  try {
-    const result = await axios.get(
-      `${process.env.REACT_APP_API_URL}/users/${studentId}`
-    );
-    console.log(result);
-    return result;
-  } catch (error) {
-    throw new Error(error.message);
-  }
+	try {
+		await axios
+			.get(`${process.env.REACT_APP_API_URL}/users/${studentId}`)
+			.then((res) => {
+				return res.data;
+			});
+	} catch (error) {
+		throw new Error(error.message);
+	}
 };
 
 export const addUser = async (
-  studentId,
-  password,
-  major,
-  name,
-  role,
-  degree,
-  comment
+	studentId,
+	password,
+	major,
+	name,
+	role,
+	degree,
+	comment
 ) => {
-  try {
-    const body = {
-      studentId,
-      password,
-      major,
-      name,
-      role,
-      degree,
-      comment,
-    };
-    if (!userValidation(body))
-      throw Error("유효성 검사에서 통과하지 못했습니다.");
-    const result = axios.post(
-      `${process.env.REACT_APP_API_URL}/users/register`,
-      body
-    );
-    return result;
-  } catch (error) {
-    throw new Error(error);
-  }
+	try {
+		const body = {
+			studentId,
+			password,
+			major,
+			name,
+			role,
+			degree,
+			comment,
+		};
+		if (!userValidation(body))
+			throw Error('유효성 검사에서 통과하지 못했습니다.');
+		await axios
+			.post(`${process.env.REACT_APP_API_URL}/users`, body)
+			.then((res) => {
+				return res.data;
+			});
+	} catch (error) {
+		throw new Error(error);
+	}
 };
 
 export const getUser = async (studentId) => {
-  try {
-    const result = await axios.get(
-      `${process.env.REACT_APP_API_URL}/users/${studentId}`
-    );
-    return result;
-  } catch (error) {
-    throw new Error(error.message);
-  }
+	try {
+    await axios.get(`${process.env.REACT_APP_API_URL}/users/${studentId}`).then((res) => {
+      return res.data;
+    });
+	} catch (error) {
+		throw new Error(error.message);
+	}
 };
 
 export const updateUser = async (id, properties) => {
-  try {
+	try {
     const body = properties;
-    const result = axios.patch(
-      `${process.env.REACT_APP_API_URL}/users/${id}`,
-
-      body
-    );
-    return result;
-  } catch (error) {
-    throw new Error(error.message);
-  }
+    await axios.patch(`${process.env.REACT_APP_API_URL}/users/${id}`, body).then((res) => {
+      return res.data;
+    });
+	} catch (error) {
+		throw new Error(error.message);
+	}
 };
